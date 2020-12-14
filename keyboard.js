9e70c380dd8136572ecf9044a500bf113a7d003f
Jack Skiles <jackskiles420@gmail.com>
2020-12-13 23:44:54 -0500

    created toggle for katakana

--- keyboard.js
+++ keyboard.js
@@ -4,16 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 let boardChange = true;
 let characterInput = "";
+
 let hiraganaAlphabet = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す',
     'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の',
     'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ',
     'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を', 'ん'];
+
 let katakanaAlphabet = ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス',
     'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', '二', 'ヌ', 'ネ', 'ノ',
     'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ',
     'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヲ', 'ン'];
+
 let alphabet = hiraganaAlphabet
-let katakana = true;
+let katakana = false;
 let newCharacters = "";
 let lastKey = "";
 let inputField = "";
@@ -22,6 +25,17 @@ function string(updated) {
 
     }
 }
+
+document.getElementById("toggle").addEventListener('click', event =>{
+    if(katakana == false){
+        katakana = true;
+        document.getElementById("toggle").innerHTML = "Hiragana";
+    } else if(katakana == true){
+        katakana = false;
+        document.getElementById("toggle").innerHTML = "Katakana";
+    }
+});
+
 document.getElementById("input").addEventListener('keydown', event => {
     //const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
     // console.log(typeof(document.getElementById("output").value));
@@ -52,8 +66,10 @@ document.getElementById("input").addEventListener('keydown', event => {
     //         newCharacters += "け"
     //         break;
     //   }
-    if (katakana == true){
+    if (katakana == true) {
         alphabet = katakanaAlphabet;
+    } else {
+        alphabet = hiraganaAlphabet;
     }
     if (characterInput == "backspace") {
         newCharacters = newCharacters.substring(0, newCharacters.length - 1);
@@ -80,12 +96,12 @@ document.getElementById("input").addEventListener('keydown', event => {
     else if (characterInput == "o") {
         newCharacters += alphabet[4];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "n") {
         newCharacters += alphabet[45];
         characterInput = "";
-    } 
-    else if (characterInput.length == 1){
+    }
+    else if (characterInput.length == 1) {
         console.log('hello');
     }
     else if (characterInput == "ka") {
@@ -127,124 +143,124 @@ document.getElementById("input").addEventListener('keydown', event => {
     else if (characterInput == "so") {
         newCharacters += alphabet[14];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ta") {
         newCharacters += alphabet[15];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "chi") {
         newCharacters += alphabet[16];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "tsu") {
         newCharacters += alphabet[17];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "te") {
         newCharacters += alphabet[18];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "to") {
         newCharacters += alphabet[19];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "na") {
         newCharacters += alphabet[20];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ni") {
         newCharacters += alphabet[21];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "nu") {
         newCharacters += alphabet[22];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ne") {
         newCharacters += alphabet[23];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "no") {
         newCharacters += alphabet[24];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ha") {
         newCharacters += alphabet[25];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "hi") {
         newCharacters += alphabet[26];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "fu") {
         newCharacters += alphabet[27];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "he") {
         newCharacters += alphabet[28];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ho") {
         newCharacters += alphabet[29];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ma") {
         newCharacters += alphabet[30];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "mi") {
         newCharacters += alphabet[31];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "mu") {
         newCharacters += alphabet[32];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "me") {
         newCharacters += alphabet[33];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "mo") {
         newCharacters += alphabet[34];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ya") {
         newCharacters += alphabet[35];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "yu") {
         newCharacters += alphabet[36];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "yo") {
         newCharacters += alphabet[37];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ra") {
         newCharacters += alphabet[38];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ri") {
         newCharacters += alphabet[39];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ru") {
         newCharacters += alphabet[40];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "re") {
         newCharacters += alphabet[41];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "ro") {
         newCharacters += alphabet[42];
         characterInput = "";
-    } 
+    }
     else if (characterInput == "wa") {
         newCharacters += alphabet[43];
         characterInput = "";
-    } 
-    else if(characterInput.length == 2){
+    }
+    else if (characterInput.length == 2) {
         console.log("what");
     } else {
         console.log("invalid input");
@@ -252,16 +268,16 @@ document.getElementById("input").addEventListener('keydown', event => {
     }
 
 
-// console.log(hiragana);
-document.getElementById("input").value = inputField;
-document.getElementById("output").value = newCharacters;
-// if(characterInput.length > 3 || lastKey == characterInput.charAt(characterInput.length - 1)){
-//     // console.log(hiragana.charAt(hiragana.length - 1));
-//     // console.log(lastKey);
-//     // console.log(hiragana);
-//     characterInput = "";
-// }
-lastKey = characterInput.charAt(characterInput.length - 1);
+    // console.log(hiragana);
+    document.getElementById("input").value = inputField;
+    document.getElementById("output").value = newCharacters;
+    // if(characterInput.length > 3 || lastKey == characterInput.charAt(characterInput.length - 1)){
+    //     // console.log(hiragana.charAt(hiragana.length - 1));
+    //     // console.log(lastKey);
+    //     // console.log(hiragana);
+    //     characterInput = "";
+    // }
+    lastKey = characterInput.charAt(characterInput.length - 1);
     // console.log(hiragana.charAt(hiragana.length - 1));
     //     console.log(lastKey);
     //     console.log(hiragana);
